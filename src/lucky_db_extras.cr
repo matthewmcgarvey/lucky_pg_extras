e50280e9ce7d10b6e9ac0ccec7229beaf6b06a05
Matthew McGarvey <matthewmcgarvey14@gmail.com>
2020-09-30 19:51:20 -0500

    Pull sql from .sql files

--- src/lucky_db_extras.cr
+++ src/lucky_db_extras.cr
@@ -16,10 +16,9 @@ module LuckyDbExtras
     {% for extra in extras %}
       def self.{{ extra.id }} : NamedTuple(column_names: Array(String), rows: Array(Array(String)))?
         LuckyDbExtras.settings.database.run do |db|
-          {% class_extra = extra.camelcase(lower: false).id %}
           column_names = [] of String
           rows = [] of Array(String)
-          db.query LuckyDbExtras::{{ class_extra }}::SQL do |rs|
+          db.query sql_for({{ extra }}) do |rs|
             return if rs.column_count.zero?
             # The first row: column names
             rs.column_count.times.each { |i| column_names << rs.column_name(i).as(String) }
@@ -38,6 +37,16 @@ module LuckyDbExtras
       end
     {% end %}
   {% end %}
+
+  def self.sql_for(query_name)
+    File.read(
+      sql_path_for(query_name)
+    )
+  end
+
+  def self.sql_path_for(query_name)
+    File.join(File.dirname(__FILE__), "/lucky_db_extras/queries/#{query_name}.sql")
+  end
 end
 
 require "../tasks/**"
--- src/lucky_db_extras/extensions.cr
+++ src/lucky_db_extras/extensions.cr
@@ -1,8 +1,3 @@
 class LuckyDbExtras::Extensions
   SUMMARY = "View enabled extensions for your database"
-  SQL = <<-SQL
-    SELECT name, default_version, installed_version, comment
-    FROM pg_available_extensions
-    ORDER BY installed_version
-  SQL
 end
